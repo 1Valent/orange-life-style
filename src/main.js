ce7586d3a71a6d519064913c4e0cdcb757da5fe4
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-04-14 01:40:22 +0300

    js btn

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// let burgerBtn = document.querySelector('.burger-btn');
-// let swgExit = document.querySelector('.swg-exit');
-// let burgerMenu = document.querySelector('.burger-menu');
-// burgerBtn.addEventListener('click', function () {
-//   burgerMenu.classList.toggle('is-open');
-// });
-// swgExit.addEventListener('click', function () {
-//   burgerMenu.classList.toggle('is-open');
-// });
+let burgerBtn = document.querySelector('.burger-btn');
+let swgExit = document.querySelector('.swg-exit');
+let burgerMenu = document.querySelector('.burger-menu');
+burgerBtn.addEventListener('click', function () {
+  burgerMenu.classList.toggle('is-open');
+});
+swgExit.addEventListener('click', function () {
+  burgerMenu.classList.toggle('is-open');
+});
