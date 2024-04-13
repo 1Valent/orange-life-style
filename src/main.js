f34c0a2145e12263127530fbc10ac6bbf6649055
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-04-13 10:02:16 +0300

    position

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// let burgerBtn = document.querySelector('.burger-btn');
+// let swgExit = document.querySelector('.swg-exit');
+// let burgerMenu = document.querySelector('.burger-menu');
+// burgerBtn.addEventListener('click', function () {
+//   burgerMenu.classList.toggle('is-open');
+// });
+// swgExit.addEventListener('click', function () {
+//   burgerMenu.classList.toggle('is-open');
+// });
